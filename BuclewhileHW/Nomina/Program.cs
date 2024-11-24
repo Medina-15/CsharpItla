35427598d5628437958c068757c9873321586b68
Medina <alisonmmr@gmail.com>
2024-11-24 01:12:25 -0400

    Buclewhile

---
+++ BuclewhileHW/Nomina/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            nomina();
+        }
+        static void nomina()
+        {
+            Console.WriteLine("Este programa calcula la cantidad de empleados que tienen un salario en un rango de 100$ a 300$ y por encima de 300 con un limite de 500");
+
+            // Declaracion de variables
+
+            int min = 100;
+            int mid = 300;
+            int max = 500;
+            int empleados = 0;
+            double totalnomina = 0;
+            double sueldo = 0;
+            int rango1 = 0;
+            int rango2 = 0;
+            string valor= string.Empty;
+            int index = 1;
+
+            // Recoleccion de datos
+
+            Console.WriteLine("Escribra la cantidad de empleados");
+            valor= Console.ReadLine();
+
+            if (!int.TryParse(valor, out empleados))
+            {
+                Console.WriteLine("Digite un numero entero");
+            }
+
+            if (empleados <= 0)
+            {
+                Console.WriteLine("El valor no puede ser negativo");
+            }
+
+            while (empleados >= index)
+            {
+                Console.WriteLine($"Escriba la el salario del empleado {index}");
+                valor = Console.ReadLine();
+
+                if (!double.TryParse(valor, out sueldo))
+                {
+                    Console.WriteLine("Debe de escribir un numero entero");
+                    continue;
+                }
+
+                if (sueldo <= max && sueldo > mid)
+                {
+                    rango1++;
+                }
+
+                else if (sueldo >= min && sueldo <= mid)
+                {
+                    rango2++;
+                }
+
+                else
+                {
+                    Console.WriteLine("Debe ingresar un monto en un rango de 100 a 500");
+                    continue;
+                }
+
+                totalnomina += sueldo;
+                index++;
+
+            }
+
+            Console.WriteLine($"La cantidad de de empleados con un sueldo entre 100 y 300 es: {rango2} y los que tienen un sueldo por encima de 300: {rango1} y la suma total de la nomina es {totalnomina}");
+        }
+    }
+}
---
+++ BuclewhileHW/notaminima/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace notaminima
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            notaminima();
+        }
+
+        private static void notaminima()
+        {
+            Console.WriteLine("Este programa recibe x cantidad de calificaciones y cuenta las que las que son iguales a la nota minima que es 7 y a las que estan por debajo de esta");
+
+            // Declarando variables
+
+            int calificaciones = 0;
+            int min = 7;
+            int notasminimas = 0;
+            int notasreprobadas = 0;
+            double nota = 0;
+            int index = 1;
+            string valor = string.Empty;
+
+            // Recoleccion de datos
+
+            Console.WriteLine("Escriba la cantidad de datos");
+            valor = Console.ReadLine();
+
+            if (!int.TryParse(valor, out calificaciones))
+            {
+                Console.WriteLine("Debe ingresar un numero entero");
+            }
+
+            if (calificaciones <= 0)
+            {
+                Console.WriteLine("El valor no puede ser igual a 0 o un numero negativo");
+            }
+
+            // Recolectando las calificaciones
+
+            while (index <= calificaciones)
+            {
+                Console.WriteLine($"Digite la nota {index} ");
+                valor= Console.ReadLine();
+
+                if (!double.TryParse(valor, out nota))
+                {
+                    Console.WriteLine("Debe ingresar un valor valido, intente de nuevo");
+                    continue;
+                }
+
+                if (nota == min)
+                {
+                    notasminimas++;
+                }
+
+                else if (nota < min)
+                {
+                    notasreprobadas++;
+                }
+
+                index++;
+            }
+
+            Console.WriteLine($"La cantidad de notas minimas son: {notasminimas} y las reprobadas son {notasreprobadas}");
+
+
+        }
+    }
+}
