f0cfe2ad7ca6715fc4f875e0a696b1b633ce407d
Medina <alisonmmr@gmail.com>
2024-11-24 13:06:58 -0400

    trianguloarea

---
+++ Triangulo/Program.cs
@@ -0,0 +1,94 @@
+﻿namespace Triangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Trianguloarea();
+        }
+
+        static void Trianguloarea()
+        {
+            Console.WriteLine("Este programa calcula el area de los triangulos, muestra sus dimensiones y luego cuenta a los que excedan un area de 12cm cuadrados.");
+
+            // Declaracion de variables
+
+            int triangulos = 0;
+
+            double Base = 0;
+            double altura = 0;
+            double area = 0;
+            double excluidos = 0;
+            string valor = string.Empty;
+
+            // Recogiendo las dimensiones del triangulo
+
+
+            try
+            {
+                Console.WriteLine("Escriba la cantidad de triangulos que quiere añadir");
+                valor = Console.ReadLine();
+
+                // Validando que los datos sean correctos
+
+                if (!int.TryParse(valor, out triangulos))
+                {
+                    Console.WriteLine("Debe de agregar un numero entero");
+                }
+
+                if (triangulos < 0)
+                {
+                    Console.WriteLine("El valor no puede ser negativo");
+                }
+
+                for (int i = 1; i <= triangulos; i++)
+                {
+                    Console.WriteLine($"Escriba la base del triangulo {i}");
+                    valor = Console.ReadLine();
+
+                    if (!double.TryParse(valor, out Base))
+                    {
+                        Console.WriteLine("El valor ingresado es incorrecto, intente de nuevo");
+                    }
+
+                    Console.WriteLine($"Escriba la altura del triangulo {i}");
+                    valor = Console.ReadLine();
+
+                    if (!double.TryParse(valor, out altura))
+                    {
+                        Console.WriteLine("El dato ingresado no es correcto");
+                    }
+                    // Operacion para encontrar el area
+
+                    area = (0.5 * Base * altura);
+
+
+
+                    // Identificando los triangulos con un area mayor a 15cm
+
+                    if (area > 12)
+                    {
+                        excluidos++;
+                    }
+
+                    Console.WriteLine($"La base del triangulo {i} es igual a {Base}, su altura es: {altura} lo que da resultado a un area de {area}");
+
+                    Console.WriteLine("------------------------------------------------------------------------------");
+
+                }
+
+                Console.WriteLine($"La cantidad de triangulos que exceden los 12 centimetros cuadrados son: {excluidos}");
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error encontrado al ingresar los datos, intente de nuevo {ex.Message}");
+            }
+
+
+
+        }
+
+
+    }
+}
